15fa3c71df0f51083eb6b9da5873586a43c2c5ab
cpintoval <cpintovalderrama@gmail.com>
2015-09-22 15:59:35 +0000

    Added links to log in, sign up and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RottenMangoes::Application.routes.draw do
-  
+
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
