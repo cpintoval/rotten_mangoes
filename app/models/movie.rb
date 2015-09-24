b4f4bf16b26f89b4c947d6fe1c2536701d0f8f48
cpintoval <cpintovalderrama@gmail.com>
2015-09-24 21:47:47 +0000

    Defined and using scopes for movie search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,6 +10,12 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validate :release_date_is_in_the_future
 
+  scope :title_match, -> (term) { where("title LIKE ?", "%#{term}%") }
+  scope :director_match, -> (term) { where("director LIKE ?", "%#{term}%") }
+  scope :runtime_under_90_minutes, -> { where("runtime_in_minutes < 90") }
+  scope :runtime_between_90_and_120_minutes, -> { where(runtime_in_minutes: 90..120) }
+  scope :runtime_above_120_minutes, -> { where("runtime_in_minutes > 120") }
+
   def review_average
     reviews.size > 0 ? reviews.sum(:rating_out_of_ten)/reviews.size : 0
   end
